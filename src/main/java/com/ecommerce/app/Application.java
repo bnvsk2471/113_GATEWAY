624b4a9f993bf025e4d44d845416ac2e7f574088
bnvsk2471 <135821034+bnvsk2471@users.noreply.github.com>
2023-09-15 16:17:57 +0530

    new

--- src/main/java/com/ecommerce/app/Application.java
+++ src/main/java/com/ecommerce/app/Application.java
@@ -2,7 +2,13 @@ package com.ecommerce.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +17,12 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+	
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer(){
+		return factory -> factory.configureDefault(
+				id->new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults()
+				).build());
+	}
 }
